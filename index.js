ff2d11acd51e2050ecc4b39cfbdf7ee1f5aaf6b7
AlexandreTheDwarf <alexandre-vandewiele@outlook.com>
2024-04-16 10:35:02 +0200

    essaie de mise en ligne sur vercel

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+////////////////////////////////
+//permettre l'accés a l'api (CORS)
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+////////////////////////////////
 
 app.get('/', (req, res)=> {
     const documentation = `
